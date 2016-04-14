3c46b1ef6dfb35e3e4858728aea9db1e51caac8a
Kyle E. Mitchell <kyle@kemitchell.com>
2016-04-13 19:47:13 -0700

    Add "MIT with Friends"

--- bin.js
+++ bin.js
@@ -74,6 +74,94 @@ workshop.add('Using The MIT License for Your Work', function() {
               mit.join('\n'),
               'Remaining lines contain standard MIT terms') }) }) }) } })
 
+workshop.add('Using The MIT License with Contributors', function() {
+  return {
+    problem: readContent('MIT-with-Friends'),
+    verify: verify(
+      { modeReset: true },
+      function(arguments, test) {
+        test.test('package.json File', function(test) {
+          test.plan(5)
+          var packageJSON = path.join(process.cwd(), 'package.json')
+          // Have a package.json file.
+          fs.stat(packageJSON, function(error, stats) {
+            test.ifError(error, 'Can stat package.json')
+            if (stats) {
+              test.equal(
+                stats.isFile(), true,
+                'Have a package.json file') } })
+          // package.json is valid JSON ...
+          fs.readFile(packageJSON, 'utf8', function(error, data) {
+            test.ifError(error, 'CAn read package.json')
+            parseJSON(data, function(error, data) {
+              test.ifError(error, 'package.json is valid JSON')
+              // with "license" of "MIT"
+              if (data) {
+                test.equal(
+                  data.license, 'MIT',
+                  '"license" is "MIT" in package.json') } }) }) })
+        var author
+        test.test('LICENSE File', function(test) {
+          test.plan(6)
+          var licenseFile = path.join(process.cwd(), 'LICENSE')
+          // Have a LICENSE file.
+          fs.stat(licenseFile, function(error, stats) {
+            test.ifError(error, 'Can stat LICENSE')
+            if (stats) {
+              test.equal(
+                stats.isFile(), true,
+                'Have a LICENSE file') } })
+          // LICENSE file content is correct.
+          fs.readFile(licenseFile, 'utf8', function(error, data) {
+            test.ifError(error, 'Can read LICENSE')
+            var lines = data
+              .replace(/\n+/, '\n')
+              .split('\n')
+            var currentYear = new Date().getFullYear().toString()
+            // License identification.
+            test.assert(
+              /^MIT License$/.exec(lines[0]),
+              'First line says "MIT License"')
+            // Copyright notice.
+            var re = new RegExp(
+              ( '^Copyright \\(c\\) ' + currentYear +
+                ' (.+) and contributors$' ))
+            var match = re.exec(lines[1])
+            test.assert(
+              !!match,
+              'Copyright notice has "and contributors"')
+            author = ( match ? match[1] : undefined )
+            // License terms.
+            test.equal(
+              lines.slice(2).join('\n').replace(/\n+/g, '\n').trim(),
+              mit.join('\n'),
+              'Remaining lines contain standard MIT terms') }) })
+        test.test('AUTHORS File', function(test) {
+          test.plan(5)
+          var licenseFile = path.join(process.cwd(), 'AUTHORS')
+          // Have an AUTHORS file.
+          fs.stat(licenseFile, function(error, stats) {
+            test.ifError(error, 'Can stat AUTHORS')
+            if (stats) {
+              test.equal(
+                stats.isFile(), true,
+                'Have an AUTHORS file') } })
+          // AUTHORS file content is correct.
+          fs.readFile(licenseFile, 'utf8', function(error, data) {
+            test.ifError(error, 'Can read AUTHORS')
+            var lines = data
+              .replace(/\n+/, '\n')
+              .split('\n')
+            // Mentions LICENSE.
+            test.assert(
+              /^#.*LICENSE file/.test(data),
+              'AUTHORS mentions the LICENSE file')
+            // Has line for author.
+            test.assert(
+              lines.some(function(line) {
+                return ( line.indexOf(author) > -1 ) }),
+              'AUTHORS has a line for the author') }) }) }) } })
+
 function readContent(basename) {
   return function() {
     return fs.createReadStream(
